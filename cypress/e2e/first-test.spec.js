d4da2b499b0dc1eff864857ff9d6e04dac234a58
mdbilalansari <mdbilalansari1234@outlook.com>
2023-10-24 17:36:52 +0530

    Web Tables

--- cypress/e2e/first-test.spec.js
+++ cypress/e2e/first-test.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress"/>
 
 const { Button } = require('bootstrap');
+const { table } = require('console');
 
 describe('first test suite', () => {
   it('first-test', () => {
@@ -201,7 +202,7 @@ describe('first test suite', () => {
       });
   });
 
-  it.only('List and Dropdown', () => {
+  it('List and Dropdown', () => {
     cy.visit('/');
 
     // 1)
@@ -219,6 +220,55 @@ describe('first test suite', () => {
         cy.wrap(dropDown).click();
       });
       cy.wrap(dropDown).click();
+    });
+  });
+
+  it.only('Web table', () => {
+    cy.visit('/');
+    cy.contains('Tables & Data').click();
+    cy.contains('Smart Table').click();
+
+    // 1) Get the row by text
+    cy.get('table')
+      .contains('tr', 'Larry')
+      .then((tableRow) => {
+        cy.wrap(tableRow).find('.nb-edit').click();
+        cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('35');
+        cy.wrap(tableRow).find('.nb-checkmark').click();
+        cy.wrap(tableRow).find('td').eq(6).should('contain', '35');
+      });
+
+    //2 Get row by index
+    cy.get('thead').find('.nb-plus').click();
+    cy.get('thead')
+      .find('tr')
+      .eq(2)
+      .then((tableRow) => {
+        cy.wrap(tableRow).find(' [placeholder="First Name"]').type('John');
+        cy.wrap(tableRow).find(' [placeholder="Last Name"]').type('Smith');
+        cy.wrap(tableRow).find('.nb-checkmark').click();
+      });
+
+    cy.get('tbody tr')
+      .first()
+      .find('td')
+      .then((tableColumns) => {
+        cy.wrap(tableColumns).eq(2).should('contain', 'John');
+        cy.wrap(tableColumns).eq(3).should('contain', 'Smith');
+      });
+
+    //3 Get each row validation
+    const age = [20, 30, 40, 200];
+    cy.wrap(age).each((age) => {
+      cy.get('thead [placeholder="Age"]').clear().type(age);
+      cy.wait(500);
+      cy.get('tbody tr').each((tableRow) => {
+        if (age == 200) {
+          cy.wrap(tableRow).should('contain', 'No data found');
+        } else {
+          cy.wrap(tableRow).find('td').eq(6).should('contain', age);
+        }
+      });
     });
   });
 });
