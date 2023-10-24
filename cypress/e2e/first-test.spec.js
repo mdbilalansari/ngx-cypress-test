908e9bffef736563cd28dd04be7f915e559a3cfa
mdbilalansari <mdbilalansari1234@outlook.com>
2023-10-24 16:04:40 +0530

    Date Picker Part 2

--- cypress/e2e/first-test.spec.js
+++ cypress/e2e/first-test.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress"/>
 
+const { Button } = require('bootstrap');
+
 describe('first test suite', () => {
   it('first-test', () => {
     //Navigating to url
@@ -164,22 +166,36 @@ describe('first test suite', () => {
   });
 
   it.only('Date Picker', () => {
+    function selectDayFromCurrent(day) {
+      let date = new Date();
+      date.setDate(date.getDate() + day);
+      let futureDay = date.getDate();
+      let futureMonth = date.toLocaleDateString('en-US', { month: 'short' });
+      let futureYear = date.getFullYear();
+      let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`;
+
+      cy.get('nb-calendar-navigation')
+        .invoke('attr', 'ng-reflect-date')
+        .then((dateAttribute) => {
+          if (!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)) {
+            cy.get('[data-name="chevron-right"]').click();
+            selectDayFromCurrent(day);
+          } else {
+            cy.get('.day-cell').not('.bounding-month').contains(futureDay).click();
+          }
+        });
+      return dateToAssert;
+    }
+
     cy.visit('/');
     cy.contains('Forms').click();
     cy.contains('Datepicker').click();
-
-    let date = new Date();
-    date.setDate(date.getDate() + 1);
-    let futureDay = date.getDate();
-    let futureMonth = date.toLocaleDateString('en-US', { month: 'short' });
-    let futureYear = date.getFullYear();
-    let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`;
 
     cy.contains('nb-card', 'Common Datepicker')
       .find('input')
       .then((input) => {
         cy.wrap(input).click();
-        cy.get('.day-cell').not('.bounding-month').contains(futureDay).click();
+        const dateToAssert = selectDayFromCurrent(600);
         cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert);
         cy.wrap(input).should('have.value', dateToAssert);
       });
